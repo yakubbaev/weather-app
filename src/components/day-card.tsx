055be38fb5186bb96fd5b07f0760a01daa105431
Mukhammad Yakubbaev <mukhammad@MacBook-Pro-Mukhammad.local>
2021-06-10 18:03:55 +0500

    Setup linters & autofix on save

---
+++ src/components/day-card.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export enum WeatherTypes {
+  Sunny,
+  Rainy,
+  Cloudy,
+  Snowy,
+}
+
+type DailyWeatherType = {
+  high: number;
+  low: number;
+  name: string;
+  weatherType: WeatherTypes;
+};
+
+export const DayCard = (prop: DailyWeatherType) => {
+  return (
+    <div>
+      <div>{prop.name}</div>
+    </div>
+  );
+};
